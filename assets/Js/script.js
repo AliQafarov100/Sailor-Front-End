8c1bcd9e2391761d905d879f99ce5243862b371a
Ali <tu8cvbxnx@code.edu.az>
2022-06-29 16:16:30 +0400

    Update commit

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -3,29 +3,35 @@ let right = document.querySelectorAll(".fa-chevron-right");
 let sliderItem = document.querySelector(".slider");
 
 left.forEach(leftBtn => {
-    leftBtn.addEventListener("click",function(){
-        let active = document.querySelector(".active");
+    leftBtn.addEventListener("click",function(e){
+        e.preventDefault();
+        let active = document.querySelector(".activeSlider");
+        
 
-        active.classList.remove("active");
+        active.classList.remove("activeSlider");
         if(active.previousElementSibling == null){
-            sliderItem.lastElementChild.classList.add("active");
+            sliderItem.lastElementChild.classList.add("activeSlider");
         }
         else{
-            active.previousElementSibling.classList.add("active");
+            active.previousElementSibling.classList.add("activeSlider");
         }
     })
 })
 
 right.forEach(rBtn => {
     rBtn.addEventListener("click",function(){
-        let active = document.querySelector(".active");
-        active.classList.remove("active");
+        
+        let active = document.querySelector(".activeSlider");
+        active.classList.remove("activeSlider");
+        // console.log(active.nextElementSibling)
 
-        if(active.nextElementSibling == null){
-            sliderItem.firstElementChild.classList.add("active");
+        if(active.nextElementSibling === null){
+            sliderItem.firstElementChild.classList.add("activeSlider");
+            console.log("first")
         }
         else{
-            active.nextElementSibling.classList.add("active");
+            active.nextElementSibling.classList.add("activeSlider");
+            console.log("next")
         }
     })
 })
